7d8a12e9d00f1de51be7e8620e72007daeb6faba
HVR22 <hectorvelardeh22@hotmail.com>
2023-04-12 07:49:26 -0500

    anadiendo HU04 y correccion de login y register

--- frontend-isw2/src/index.js
+++ frontend-isw2/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
 import HU009PaginaPrincipal from './pages/HU009-PaginaPrincipal';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+
+import HU004Profesor from './pages/HU004Profesor';
+
+
+
 //import Navbar from './componentes/navbar';
 
 
@@ -19,9 +25,43 @@ root.render(
     <BrowserRouter>
     
     <Routes>
+
+
+
+
+
+
+
+
+
+
       <Route path='/PaginaPrincipal' element={<HU009PaginaPrincipal />}/>
+
+
+
+
+
+
+
+
+
+
+
+
+
       <Route path='/Register' element={<Register/>}/>
       <Route path='/' element={<Login/>}/>
+
+
+
+
+
+
+
+
+
+
+      <Route path='/AsignadosProfesor' element={<HU004Profesor/>}/>
       
       
     </Routes>
